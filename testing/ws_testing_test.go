5a62a204f6c10ead77ca4002c3ff3f2f2ec9735f
Themba <thembangubeni04@gmail.com>
2025-02-20 02:01:59 +0200

    Update ws_testing_test.go

--- testing/ws_testing_test.go
+++ testing/ws_testing_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestTestingWs(t *testing.T) {
-	ws := NewWs(NewTestCase(t, http.Server("127.0.0.1", 0).SetMaxWebsocketPayload(int(math.Pow(2, 16)+2)), true))
+	ws := NewWs(NewTestCase(t, http.Server("127.0.0.1", 0).SetMaxWebsocketPayload(int(math.Pow(2, 16))), true))
 
 	type coordinate struct {
 		Longitude float32 `json:"longitude"`
@@ -81,7 +81,7 @@ func TestTestingWs(t *testing.T) {
 	}
 
 	// Length greater then 126 less then 2^16
-	data2__16 := strings.Random(int(math.Pow(2, 16)) + 1)
+	data2__16 := strings.Random(int(math.Pow(2, 16)))
 
 	res.WriteText([]byte(data2__16))
 
