9663e08631ad9b8d036913613cefd9b4527cc599
lucas11776 <lucas11776>
2025-04-30 16:47:54 +0200

    Update: Removed death code.

--- server_web_test.go
+++ server_web_test.go
@@ -1,29 +1,11 @@
 package http
 
-// import (
-// 	"io/fs"
-// 	"math/rand"
-// 	"net/url"
-// 	"strconv"
-// 	"strings"
-// 	"sync"
-// 	"testing"
-
-// 	"github.com/lucas11776-golang/http/types"
-// 	"github.com/lucas11776-golang/http/utils/reader"
-// 	req "github.com/lucas11776-golang/http/utils/request"
-// 	str "github.com/lucas11776-golang/http/utils/strings"
-// 	"github.com/open2b/scriggo"
-// )
-
 import (
 	"encoding/json"
 	"io"
-	"io/fs"
 	"math/rand"
 	"strconv"
 	"strings"
-	"sync"
 	"testing"
 
 	"github.com/lucas11776-golang/http/types"
@@ -34,62 +16,22 @@ import (
 )
 
 func TestServerWeb(t *testing.T) {
+	var AuthKey = "KEY-" + strconv.Itoa(int(rand.Float32()*10000))
+
 	type Message struct {
 		Message string `json:"message"`
+	}
+
+	type User struct {
+		ID    int64  `json:"id"`
+		Role  byte   `json:"role"`
+		Email string `json:"email"`
 	}
 
 	users := []User{
 		(User{ID: 1, Role: 1, Email: "jane@doe.com"}),
 		(User{ID: 2, Role: 0, Email: "jeo@doe.com"}),
 	}
-
-	// serve := func() *HTTP {
-	// 	server := Server("127.0.0.1", 0)
-
-	// 	server.Route().Group("api", func(route *Router) {
-	// 		route.Group("users", func(route *Router) {
-	// 			// route.Get("/", func(req *Request, res *Response) *Response {
-	// 			// 	return res.Json(users)
-	// 			// })
-	// 			route.Post("/", func(req *Request, res *Response) *Response {
-	// 				return res.SetStatus(HTTP_RESPONSE_OK).Json(userCreatedMessage)
-	// 			}).Middleware(AuthorizationGuard)
-	// 			route.Group("{id}", func(route *Router) {
-	// 				route.Get("/", func(req *Request, res *Response) *Response {
-	// 					return res
-	// 				})
-	// 			})
-	// 		})
-	// 	})
-
-	// 	server.Route().Group("authentication", func(route *Router) {
-	// 		route.Middleware(IsGuest).Group("login", func(route *Router) {
-	// 			route.Get("/", func(req *Request, res *Response) *Response {
-	// 				return res.Html("<h1>Login page</h1>")
-	// 			})
-	// 			route.Post("/", func(req *Request, res *Response) *Response {
-	// 				user := users[0]
-
-	// 				res.Session.Set("user_id", strconv.Itoa(int(user.ID)))
-	// 				res.Session.Set("role", strconv.Itoa(int(user.Role)))
-
-	// 				return res.Redirect("dashboard")
-	// 			})
-	// 		})
-	// 	})
-
-	// 	server.Route().Middleware(IsUser, IsAdmin).Group("dashboard", func(route *Router) {
-	// 		route.Get("/", func(req *Request, res *Response) *Response {
-	// 			html := strings.Join([]string{"<h1>Welcome to dashboard user ", req.Session.Get("user_id"), "</h1>"}, "")
-
-	// 			return res.Html(html)
-	// 		})
-	// 	})
-
-	// 	go server.Listen()
-
-	// 	return server
-	// }
 
 	t.Run("TestApiGetUsers", func(t *testing.T) {
 		server := Server("127.0.0.1", 0)
@@ -150,7 +92,7 @@ func TestServerWeb(t *testing.T) {
 		}
 
 		auth := func(req *Request, res *Response, next Next) *Response {
-			if req.GetHeader("authorization") != authKey {
+			if req.GetHeader("authorization") != AuthKey {
 				return res.SetStatus(HTTP_RESPONSE_UNAUTHORIZED).Json(unauthorizedMessage)
 			}
 			return next()
@@ -205,7 +147,7 @@ func TestServerWeb(t *testing.T) {
 			SetHeaders(types.Headers{
 				"content-type":  "application/json",
 				"host":          "127.0.0.1:4567",
-				"authorization": authKey,
+				"authorization": AuthKey,
 			})
 
 		http, err = r.Post(strings.Join([]string{"http://", server.Host(), "/api/users"}, ""), []byte{})
@@ -290,6 +232,15 @@ func TestServerWeb(t *testing.T) {
 
 		server.Session([]byte(str.Random(10)))
 
+		// Comment
+		IsGuest := func(req *Request, res *Response, next Next) *Response {
+			if req.Session.Get("user_id") != "" {
+				return res.Redirect("/")
+			}
+
+			return next()
+		}
+
 		server.Route().Group("authentication", func(route *Router) {
 			route.Middleware(IsGuest).Group("login", func(route *Router) {
 				route.Get("/", func(req *Request, res *Response) *Response {
@@ -352,95 +303,4 @@ func TestServerWeb(t *testing.T) {
 
 		server.Close()
 	})
-}
-
-type Message struct {
-	Message string `json:"message"`
-}
-
-var authKey = "KEY-" + strconv.Itoa(int(rand.Float32()*10000))
-
-type User struct {
-	ID    int64  `json:"id"`
-	Role  byte   `json:"role"`
-	Email string `json:"email"`
-}
-
-var unauthorizedAccessMessage = Message{
-	Message: "Authorization key is invalid",
-}
-
-var userCreatedMessage = Message{
-	Message: "Authorization key is invalid",
-}
-
-// Comment
-func AuthorizationGuard(req *Request, res *Response, next Next) *Response {
-	if req.GetHeader("authorization") != authKey {
-		return res.SetStatus(HTTP_RESPONSE_UNAUTHORIZED).Json(unauthorizedAccessMessage)
-	}
-
-	return next()
-}
-
-var cssNameWebServer = "assets/css/main.css"
-
-var cssContentWebServer = strings.Join([]string{
-	"body { margin: 0px !important; padding: 0px !important; background-color: green; }",
-}, "\r\n")
-
-var webServerReaderTestFS = scriggo.Files{
-	cssName: []byte(cssContentWebServer),
-}
-
-type webServerReaderTest struct {
-	mutex sync.Mutex
-	cache scriggo.Files
-}
-
-// Comment
-func (ctx *webServerReaderTest) Open(name string) (fs.File, error) {
-	return webServerReaderTestFS.Open(name)
-}
-
-// Comment
-func (ctx *webServerReaderTest) Cache(name string) (scriggo.Files, error) {
-	return reader.ReadCache(ctx, ctx.cache, name)
-}
-
-// Comment
-func (ctx *webServerReaderTest) Write(name string, data []byte) error {
-	ctx.mutex.Lock()
-
-	ctx.cache[name] = data
-
-	ctx.mutex.Unlock()
-
-	return nil
-}
-
-// Comment
-func IsGuest(req *Request, res *Response, next Next) *Response {
-	if req.Session.Get("user_id") != "" {
-		return res.Redirect("/")
-	}
-
-	return next()
-}
-
-// Comment
-func IsUser(req *Request, res *Response, next Next) *Response {
-	if req.Session.Get("user_id") == "" {
-		return res.Redirect("authentication/login")
-	}
-
-	return next()
-}
-
-// Comment
-func IsAdmin(req *Request, res *Response, next Next) *Response {
-	if req.Session.Get("role") != "1" {
-		return res.Redirect("/")
-	}
-	return next()
 }
